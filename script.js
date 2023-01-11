8592f45785ad1e7c87a719a6df0f94a622523b02
Chris <chriscablish@hotmail.com>
2023-01-11 13:28:10 -0600

    generate rand numbers and retrieve elements from html

---
+++ script.js
@@ -0,0 +1,8 @@
+const randnum1 = Math.ceil(Math.random * 10);
+const randnum2 = Math.ceil(Math.random * 10);
+
+const formEl = document.getElementById('form');
+const scoreEl = document.getElementById('score');
+const questionEl = document.getElementById('question');
+const inputEl = document.getElementById('input');
+
