c364b5d8a261929f6dc8e02c4ecfd1d7c2a46e12
Chris <chriscablish@hotmail.com>
2023-01-16 12:48:10 -0600

    adjust number range to exclude 1 and 10

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const randNum1 = Math.ceil(Math.random() * 10);
-const randNum2 = Math.ceil(Math.random() * 10);
+//const randNum1 = Math.ceil(Math.random() * 10);
+//const randNum2 = Math.ceil(Math.random() * 10);
+
+const randNum1 = Math.floor(Math.random() * (9 - 2 + 1) + 2);
+const randNum2 = Math.floor(Math.random() * (9 - 2 + 1) + 2);
 
 const formEl = document.getElementById('form');
 const scoreEl = document.getElementById('score');
@@ -17,8 +20,6 @@ questionEl.innerText = `What is ${randNum1} multiplied by ${randNum2}?`
 const correctAns = randNum1 * randNum2
 
 scoreEl.innerText = `Score: ${score}`;
-
-console.log(+inputEl.value)
 
 formEl.addEventListener('submit', ()=>{
     const userAns = +inputEl.value
@@ -38,12 +39,51 @@ const updateScoreDisplay = ()=> {
 
 /*peek section*/
 
+
+/* save the span with the revealed correct answer to the answerEl variable. If,when the code runs, the display style of answerEl is 'none', then it should be set equal to block instead. If display style is NOT set to none, it should be switched to none. The inner text of answerEl should be set equal to the variable correctAns THIS ONLY TAKES PLACE AFTER <button> has been clicked. the function is triggered by onclick event*/
+
+
+/*shows correct answer when button is pushed*/
+
 function showAndHide() {
-    const x = document.getElementById('answer');
-    if (x.style.display == 'none') {
-        x.style.display = 'block';
+    const answerEl = document.getElementById('answer');
+    if (answerEl.style.display == 'none') {
+        answerEl.style.display = 'block';
     } else {
-        x.style.display = 'none';
+        answerEl.style.display = 'none';
     }
+    answerEl.innerText = correctAns;
 }
+
+let count = JSON.parse(sessionStorage.getItem('count'));
+
+if (count === null) {
+    count = 0;
+}
+
+if (count >= 5) {
+    const showEl = document.getElementById('show');
+    showEl.style.display = 'none';
+}
+
+document.getElementById('peeks').innerText = `Peeks used: ${count}/5`
+
+const counterFunc = ()=> {
+    count++;
+    document.getElementById('peeks').innerText = `Peeks used: ${count}/5`
+    updateSessionStoragePeeks();
+}
+
+const updateSessionStoragePeeks = ()=> {
+    sessionStorage.setItem('count', JSON.stringify(count));
+}
+
+
+
+
+
+
+
+
+
 
